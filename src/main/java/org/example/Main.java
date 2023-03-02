2c1f5008702f080107985b205b9f4a3cabe87a7d
깃ChertHyeon <r4560798@naver.com>
2023-03-02 10:02:46 +0900

    3단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        String input;
+
+        Map<Integer, 명언> 명언저장소 = new HashMap<>();
+        int count = 1;
 
         String famous;
         String writer;
 
         while (true) {
+            System.out.print("명령) ");
+            input = sc.nextLine();
             if (input.equals("종료")) {
                 break;
             }
@@ -20,11 +24,24 @@ public class Main {
                 System.out.print("명언 : ");
                 famous = sc.nextLine();
                 System.out.print("작가 : ");
-                famous = sc.nextLine();
-                System.out.print("명령) ");
-                input = sc.nextLine();
+                writer = sc.nextLine();
+                명언저장소.put(count, new 명언(famous, writer));
+                System.out.println(count + "번 명언이 등록되었습니다.");
+                count++;
+
             }
 
         }
+        sc.close();
+    }
+}
+
+class 명언 {
+    private String famous;
+    private String writer;
+
+    명언(String famous, String writer) {
+        this.famous = famous;
+        this.writer = writer;
     }
 }
